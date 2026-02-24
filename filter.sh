ccc45e860206f1540f5cf7d03aedd9a4bf79ba3b
aaron <communicate_aaron@outlook.com>
2026-02-24 23:02:23 +0800

    feat: 修订文档与脚本逻辑，优化引用与格式 :memo: :art: :wrench:
    - :memo: 修订《第 3 章 模块化》文档中的代码参考链接，更新为 `Vector.cpp`。
    - :memo: 在《进一步阅读》文档添加《第 3 章 模块化》的章节链接。
    - :art: 调整 README.md，移除重复的文件说明，优化内容排版。
    - :wrench: 更新 `filter.sh` 脚本，新增处理单星号斜体样式的逻辑，并优化正则匹配。

--- filter.sh
+++ filter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 1. 处理 ==text== 为 @mark{text}
-# 2. 将 **text** 转换为 <b>text</b>
+# 2. 将 **text** 转换为 <b>text</b>，处理 * * 为 <i> </i>
 # 3. 将 <!-- id:xxx --> \n # Title 转换为 生成页面定义 + 原标题（用于建立页面层级）：\n#    @page <id> <Title>  +  # Title
 # 4. 将 [text](../path/filename.cpp) 转换为 @ref filename.cpp "text"
 # 5. 将 [text](#anchor) 转换为 @ref anchor "text"
@@ -16,4 +16,4 @@ elif [ "$FILENAME" == "further_reading.md" ]; then
 fi
 
 # 使用 perl 方便处理跨行
-echo -e "$CONTENT" | perl -0777 -pe 's/==([^=]*)==/\@mark{$1}/g; s/\*\*(.*?)\*\*/<b>$1<\/b>/g; s/<!-- id:(\S+) -->\s*\n\s*(#+)\s*(.*)/\@page $1 $3/g; s/\[([^\]]+)\]\(\.\.\/[^\)]+\/([^\/\)]+)\)/\@ref $2 "$1"/g; s/\[([^\]]+)\]\(#([^ \)]+)\)/\@ref $2 "$1"/g; s/\[([^\]]+)\]\(\.\/1_基础.md\)/\@ref basics_doc "$1"/g'
+echo -e "$CONTENT" | perl -0777 -pe 's/==([^=]*)==/\@mark{$1}/g; s/\*\*(.*?)\*\*/<b>$1<\/b>/g; s/(?<!\*)\*([^\*\n]+?)\*(?!\*)/<i>$1<\/i>/g; s/<!-- id:(\S+) -->\s*\n\s*(#+)\s*(.*)/$2 $3 {#$1}/g; s/\[([^\]]+)\]\(\.\.\/[^\)]+\/([^\/\)]+)\)/\@ref $2 "$1"/g; s/\[([^\]]+)\]\(#([^ \)]+)\)/\@ref $2 "$1"/g; s/\[([^\]]+)\]\(\.\/1_基础.md\)/\@ref basics_doc "$1"/g'
